19b6beac83937bdcb6b70da21b3d8a7c8c1f3daa
TOTTI <jfpetrelli@gmail.com>
2022-12-21 20:57:39 -0300

    .

--- index.php
+++ index.php
@@ -1,26 +1,68 @@
 <?php include("templates/header.php"); ?>
 
 <div class="container text-center">
-<div class="row">
-        <div class="col-6 ">
-            <div class="p-3 border bg-light">
-                <img src="./img/img1.jpg" alt="" srcset="" class = "img-fluid">
- 
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere libero consequuntur deserunt quam nostrum eius inventore voluptatem corrupti nesciunt illo officiis, voluptates aspernatur soluta necessitatibus earum sit, quae commodi praesentium!
-                Libero similique odio molestias, modi quia soluta impedit culpa quam quae exercitationem nesciunt pariatur quidem quaerat obcaecati amet magnam repellendus, deserunt ab, fugit ipsa vero eveniet! Ut quisquam accusantium nemo?
-                Ipsum cum suscipit deserunt autem tempora. Molestias quibusdam nobis officiis ad quidem deserunt corporis ducimus praesentium, numquam nihil sapiente excepturi maxime facere non officia aspernatur consectetur velit, eius, nemo dolorum.
-            </div>
-        </div>
-        <div class="col-6 ">
-            <div class="p-3 border bg-light">
-                <img src="./img/img1.jpg" alt="" srcset="" class = "img-fluid">
- 
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere libero consequuntur deserunt quam nostrum eius inventore voluptatem corrupti nesciunt illo officiis, voluptates aspernatur soluta necessitatibus earum sit, quae commodi praesentium!
-                Libero similique odio molestias, modi quia soluta impedit culpa quam quae exercitationem nesciunt pariatur quidem quaerat obcaecati amet magnam repellendus, deserunt ab, fugit ipsa vero eveniet! Ut quisquam accusantium nemo?
-            </div>
+    <div class="row justify-content-center">
+            <div class="col-5 border bg-light m-2">
+                <div class="p-3 ">
+                    <img src="./img/img1.jpg" alt="" srcset="" class = "img-fluid">
     
-        </div>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere libero consequuntur deserunt quam nostrum eius inventore voluptatem corrupti nesciunt illo officiis, voluptates aspernatur soluta necessitatibus earum sit, quae commodi praesentium!
+                    Libero similique odio molestias, modi quia soluta impedit culpa quam quae exercitationem nesciunt pariatur quidem quaerat obcaecati amet magnam repellendus, deserunt ab, fugit ipsa vero eveniet! Ut quisquam accusantium nemo?
+                    Ipsum cum suscipit deserunt autem tempora. Molestias quibusdam nobis officiis ad quidem deserunt corporis ducimus praesentium, numquam nihil sapiente excepturi maxime facere non officia aspernatur consectetur velit, eius, nemo dolorum.
+                </div>
+            </div>
+            <div class="col-5 border bg-light m-2">
+                <div class="p-3 ">
+                    <img src="./img/img2.jpg" alt="" srcset="" class = "img-fluid">
+    
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere libero consequuntur deserunt quam nostrum eius inventore voluptatem corrupti nesciunt illo officiis, voluptates aspernatur soluta necessitatibus earum sit, quae commodi praesentium!
+                    Libero similique odio molestias, modi quia soluta impedit culpa quam quae exercitationem nesciunt pariatur quidem quaerat obcaecati amet magnam repellendus, deserunt ab, fugit ipsa vero eveniet! Ut quisquam accusantium nemo?
+                </div>
+
+            </div>
+    </div>
+
+    <div class="row justify-content-center">
+            <div class="col-5 border bg-light m-2">
+                <div class="p-3 text-center">
+                    <img src="./img/img3.jpg" alt="" srcset="" class = "img-center">
+    
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere libero consequuntur deserunt quam nostrum eius inventore voluptatem corrupti nesciunt illo officiis, voluptates aspernatur soluta necessitatibus earum sit, quae commodi praesentium!
+                    Libero similique odio molestias, modi quia soluta impedit culpa quam quae exercitationem nesciunt pariatur quidem quaerat obcaecati amet magnam repellendus, deserunt ab, fugit ipsa vero eveniet! Ut quisquam accusantium nemo?
+                    Ipsum cum suscipit deserunt autem tempora. Molestias quibusdam nobis officiis ad quidem deserunt corporis ducimus praesentium, numquam nihil sapiente excepturi maxime facere non officia aspernatur consectetur velit, eius, nemo dolorum.
+                </div>
+            </div>
+            <div class="col-5 border bg-light m-2">
+                <div class="p-3 text-center">
+                    <img src="./img/img5.jpg" alt="" srcset="" class = "img-fluid">
+    
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere libero consequuntur deserunt quam nostrum eius inventore voluptatem corrupti nesciunt illo officiis, voluptates aspernatur soluta necessitatibus earum sit, quae commodi praesentium!
+                    Libero similique odio molestias, modi quia soluta impedit culpa quam quae exercitationem nesciunt pariatur quidem quaerat obcaecati amet magnam repellendus, deserunt ab, fugit ipsa vero eveniet! Ut quisquam accusantium nemo?
+                </div>
+
+            </div>
+    </div>
+    
+    <div class="row justify-content-center">
+            <div class="col-5 border bg-light m-2">
+                <div class="p-3 ">
+                    <img src="./img/img5.jpg" alt="" srcset="" class = "img-fluid">
+    
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere libero consequuntur deserunt quam nostrum eius inventore voluptatem corrupti nesciunt illo officiis, voluptates aspernatur soluta necessitatibus earum sit, quae commodi praesentium!
+                    Libero similique odio molestias, modi quia soluta impedit culpa quam quae exercitationem nesciunt pariatur quidem quaerat obcaecati amet magnam repellendus, deserunt ab, fugit ipsa vero eveniet! Ut quisquam accusantium nemo?
+                    Ipsum cum suscipit deserunt autem tempora. Molestias quibusdam nobis officiis ad quidem deserunt corporis ducimus praesentium, numquam nihil sapiente excepturi maxime facere non officia aspernatur consectetur velit, eius, nemo dolorum.
+                </div>
+            </div>
+            <div class="col-5 border bg-light m-2">
+                <div class="p-3 ">
+                    <img src="./img/img6.jpg" alt="" srcset="" class = "img-fluid">
+    
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere libero consequuntur deserunt quam nostrum eius inventore voluptatem corrupti nesciunt illo officiis, voluptates aspernatur soluta necessitatibus earum sit, quae commodi praesentium!
+                    Libero similique odio molestias, modi quia soluta impedit culpa quam quae exercitationem nesciunt pariatur quidem quaerat obcaecati amet magnam repellendus, deserunt ab, fugit ipsa vero eveniet! Ut quisquam accusantium nemo?
+                </div>
+
+            </div>
+    </div>
+
 </div>
-
-
 <?php include("templates/footer.php"); ?>

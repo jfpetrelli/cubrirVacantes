4e100e2d1b694ba3ddbfc66741b3f29f30800262
TOTTI <jfpetrelli@gmail.com>
2022-12-21 18:34:33 -0300

    create

---
+++ templates/footer.php
@@ -0,0 +1,14 @@
+
+</body>
+
+<footer class="page-footer bg-primary">
+<div class="container">
+
+
+  <div class="footer-copyright text-center py-3 fixed-bottom bg-primary">© 2023 Copyright:
+    <a class="navbar-brand" href="https://www.frro.utn.edu.ar/"> www.frro.utn.edu.ar</a>
+  </div>
+
+</footer>
+
+</html>
---
+++ templates/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="./css/bootstrap.css">
+</head>
+<header class="fixed-top">
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">Cubrir Vacantes</a>
+    <div class="d-flex justify-content-end" id="navbarColor01">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Informacion
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Ayuda</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contacto</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Iniciar Sesion</a>
+        </li>
+    </ul>
+    </div>
+  </div>
+</nav>
+
+</header>
+
+
+<body class="pt-5 mt-5 mb-5 pb-3">
+    

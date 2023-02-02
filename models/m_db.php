fcf7a3a09463123ab8cca565d81e60d7bf2ea99d
TOTTI <jfpetrelli@gmail.com>
2023-02-02 17:26:05 -0300

    .

--- models/m_db.php
+++ models/m_db.php
@@ -10,23 +10,7 @@
            return $con;
 
         }
-/*
-        public function getUsers(){
-            
-            $query = $this->con->query('SELECT * FROM users');
 
-            $res = [];
-            $i = 0;
-
-            while($row = $query->fetch_assoc()){
-                $res[$i] = $row;
-                $i++;
-            }
-
-            return $res;
-        }
-
-*/
     }
 
 

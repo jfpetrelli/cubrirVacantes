3f58fed36760c3eb4e1cab3ac73b31604b76195b
ToTTi <jfpetrelli@gmail.com>
2023-08-23 00:36:39 -0300

    v 2.3

--- config/config.php
+++ config/config.php
@@ -1,10 +1,13 @@
 <?php
     
-        
+//BASE DE DATOS LOCAL        
 $dbhost = 'localhost';
 $dbuser = 'root';
 $dbpass = 'root';
 $dbname = 'cubrir_vacantes';
 
+//DATOS MAIL  (Reemplazar por mail origen)
+$user_mail = 'xxxxxxx@gmail.com';
+$pass_mail = 'xxxxxxx';
 
 ?>

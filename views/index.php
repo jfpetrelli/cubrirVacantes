336c8de0db9ae382996304c6cc85258c014c4464
ToTTi <jfpetrelli@gmail.com>
2024-05-06 21:09:22 -0300

    .

--- views/index.php
+++ views/index.php
@@ -11,7 +11,7 @@
         <div class="col-5 border bg-light m-2">
             <div class="p-3 text-center">
                 <h3>Titulo 1</h3>
-                <img src="./img/img1.jpg" alt="" class = "mx-auto d-block img-fluid">
+                <img src="./img/img1.jpg" alt="Imagen 1" class = "mx-auto d-block img-fluid">
                 <p lang="zxx">
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere libero consequuntur deserunt quam nostrum eius inventore voluptatem corrupti nesciunt illo officiis, voluptates aspernatur soluta necessitatibus earum sit, quae commodi praesentium!
                 Libero similique odio molestias, modi quia soluta impedit culpa quam quae exercitationem nesciunt pariatur quidem quaerat obcaecati amet magnam repellendus, deserunt ab, fugit ipsa vero eveniet! Ut quisquam accusantium nemo?
@@ -22,7 +22,7 @@
         <div class="col-5 border bg-light m-2">
             <div class="p-3 text-center">
                 <h3>Titulo 2</h3>
-                <img src="./img/img2.jpg" alt="" class = "mx-auto d-block img-fluid">
+                <img src="./img/img2.jpg" alt="Imagen 2" class = "mx-auto d-block img-fluid">
                 <p lang="zxx">
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere libero consequuntur deserunt quam nostrum eius inventore voluptatem corrupti nesciunt illo officiis, voluptates aspernatur soluta necessitatibus earum sit, quae commodi praesentium!
                 Libero similique odio molestias, modi quia soluta impedit culpa quam quae exercitationem nesciunt pariatur quidem quaerat obcaecati amet magnam repellendus, deserunt ab, fugit ipsa vero eveniet! Ut quisquam accusantium nemo?
@@ -36,7 +36,7 @@
         <div class="col-5 border bg-light m-2">
             <div class="p-3 text-center">
                 <h3>Titulo 3</h3>
-                <img src="./img/img3.jpg" alt="" class = "mx-auto d-block img-fluid">
+                <img src="./img/img3.jpg" alt="Imagen 3" class = "mx-auto d-block img-fluid">
                 <p lang="zxx">
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere libero consequuntur deserunt quam nostrum eius inventore voluptatem corrupti nesciunt illo officiis, voluptates aspernatur soluta necessitatibus earum sit, quae commodi praesentium!
                 Libero similique odio molestias, modi quia soluta impedit culpa quam quae exercitationem nesciunt pariatur quidem quaerat obcaecati amet magnam repellendus, deserunt ab, fugit ipsa vero eveniet! Ut quisquam accusantium nemo?
@@ -47,7 +47,7 @@
         <div class="col-5 border bg-light m-2">
             <div class="p-3 text-center">
                 <h3>Titulo 4</h3>
-                <img src="./img/img5.jpg" alt="" class = "mx-auto d-block img-fluid">
+                <img src="./img/img5.jpg" alt="Imagen 4" class = "mx-auto d-block img-fluid">
                 <p lang="zxx">
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere libero consequuntur deserunt quam nostrum eius inventore voluptatem corrupti nesciunt illo officiis, voluptates aspernatur soluta necessitatibus earum sit, quae commodi praesentium!
                 Libero similique odio molestias, modi quia soluta impedit culpa quam quae exercitationem nesciunt pariatur quidem quaerat obcaecati amet magnam repellendus, deserunt ab, fugit ipsa vero eveniet! Ut quisquam accusantium nemo?
@@ -60,7 +60,7 @@
         <div class="col-5 border bg-light m-2">
             <div class="p-3 text-center">
                 <h3>Titulo 5</h3>
-                <img src="./img/img5.jpg" alt="" class = "mx-auto d-block img-fluid">
+                <img src="./img/img5.jpg" alt="Imagen 5" class = "mx-auto d-block img-fluid">
                 <p lang="zxx">
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere libero consequuntur deserunt quam nostrum eius inventore voluptatem corrupti nesciunt illo officiis, voluptates aspernatur soluta necessitatibus earum sit, quae commodi praesentium!
                 Libero similique odio molestias, modi quia soluta impedit culpa quam quae exercitationem nesciunt pariatur quidem quaerat obcaecati amet magnam repellendus, deserunt ab, fugit ipsa vero eveniet! Ut quisquam accusantium nemo?
@@ -71,7 +71,7 @@
         <div class="col-5 border bg-light m-2">
             <div class="p-3 text-center">
                 <h3>Titulo 6</h3>
-                <img src="./img/img6.jpg" alt="" class = "mx-auto d-block img-fluid">
+                <img src="./img/img6.jpg" alt="Imagen 6" class = "mx-auto d-block img-fluid">
                 <p lang="zxx">
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere libero consequuntur deserunt quam nostrum eius inventore voluptatem corrupti nesciunt illo officiis, voluptates aspernatur soluta necessitatibus earum sit, quae commodi praesentium!
                 Libero similique odio molestias, modi quia soluta impedit culpa quam quae exercitationem nesciunt pariatur quidem quaerat obcaecati amet magnam repellendus, deserunt ab, fugit ipsa vero eveniet! Ut quisquam accusantium nemo?
